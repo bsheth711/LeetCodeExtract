050f4d30ee9e7099470503f68c29afec1858ec89
Benjamin Sheth <bsheth@wisc.edu>
2024-08-26 23:03:33 -0500

    enclosed script variables

--- src/run.sh
+++ src/run.sh
@@ -7,14 +7,14 @@ localFolder="../REPO"
 
 cd "$(dirname "$0")"
 
-if ! test -d $localFolder; then
-	git clone $githubRepo $localFolder
+if ! test -d "${localFolder}"; then
+	git clone "${githubRepo}" "${localFolder}"
 	echo -e "# Leetcode Solutions\nThis repository is programatically generated and updated by [LeetCodeExtract](https://github.com/bsheth711/LeetCodeExtract)." > "${localFolder}/README.md"
 fi
 
 # FYI: a unix style path does not work from windows mingw environment variable path for node.exe
 node main.js
-cd $localFolder
+cd "${localFolder}"
 git add .
 git commit -m "update"
 git push
